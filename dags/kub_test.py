448a2ca02b7e8c2ff316e9e015ca1580a45a035e
pavanguru <pavan@propertyguru.com.sg>
2019-10-07 12:45:52 +0800

    modifed kube dag to fix

--- dags/kub_test.py
+++ dags/kub_test.py
@@ -1,20 +1,31 @@
-import datetime
-from airflow import models
-from airflow.contrib.kubernetes import pod
-from airflow.contrib.kubernetes import secret
-from airflow.contrib.operators import kubernetes_pod_operator
+import airflow
+from airflow import DAG
+from airflow.contrib.operators import KubernetesOperator
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.contrib.kubernetes.secret import Secret
 
 
+default_args = {
+    'start_date': airflow.utils.dates.days_ago(0),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
 
-with DAG('test_kube', default_args=default_args, description='Kubernetes Operator',
-         schedule_interval='00 12 01 * *') as dag:
-    k = KubernetesPodOperator(namespace='default',
-                              image="gcr.io/propertyguru-datalake-v0/dsa/airflow:python37", #Image path was incorrect
-                              name="test",
-                              in_cluster=True, #To trigger cluster kubeconfig.
-                              image_pull_policy="Always",  #In my case, I need the image update to occur whenever there is an update
-                              task_id="test",
-                              is_delete_operator_pod=False,
-                              hostnetwork=False,
-                              dag=dag
-                              )
+dag = DAG(
+    'test_kube',
+    default_args=default_args,
+    description='Kubernetes Operator desc dag',
+    schedule_interval=None,
+    dagrun_timeout=timedelta(minutes=60))
+
+
+k = KubernetesPodOperator(namespace='default',
+                          image="gcr.io/propertyguru-datalake-v0/dsa/airflow:python37", #Image path was incorrect
+                          name="test",
+                          in_cluster=True, #To trigger cluster kubeconfig.
+                          image_pull_policy="Always",  #In my case, I need the image update to occur whenever there is an update
+                          task_id="test",
+                          is_delete_operator_pod=False,
+                          hostnetwork=False,
+                          dag=dag
+                          )
