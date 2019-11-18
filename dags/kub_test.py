bd58e8bd04b58246bf35fcf24865d5578f9651df
pavanguru <pavan@propertyguru.com.sg>
2019-11-18 15:44:30 +0800

    added secret keys

--- dags/kub_test.py
+++ dags/kub_test.py
@@ -3,7 +3,6 @@ from airflow import DAG
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.contrib.kubernetes.secret import Secret
 from datetime import timedelta
-from airflow.operators.slack_operator import SlackAPIPostOperator
 
 
 default_args = {
@@ -19,12 +18,12 @@ dag = DAG(
     schedule_interval=None,
     dagrun_timeout=timedelta(minutes=60))
 
-# secret_env = Secret(
-#     deploy_type = 'env',
-#     deploy_target = 'GOOGLE_APPLICATION_CREDENTIALS',
-#     secret = 'airflow-testing-secret',
-#     key = 'key.json'
-# )
+secret_env = Secret(
+    deploy_type = 'env',
+    deploy_target = 'GOOGLE_APPLICATION_CREDENTIALS',
+    secret = 'airflow-testing-secret',
+    key = 'key.json'
+)
 
 
 
@@ -33,7 +32,7 @@ py_test_k8_task1 = KubernetesPodOperator(namespace='airflow-test',
                           cmd=["/bin/bash", "-c"],
                           arguments=["python", "./prog/py_test.py"],
                           name="test-kube",
-                          # secrets=[secret_env],
+                          secrets=[secret_env],
                           in_cluster=True, #To trigger cluster kubeconfig.
                           image_pull_policy="Always",  #In my case, I need the image update to occur whenever there is an update
                           task_id="py_test_k8_task1",
@@ -48,7 +47,7 @@ py_test_k8_task2 = KubernetesPodOperator(namespace='airflow-test',
                           cmd=["/bin/bash", "-c"],
                           arguments=["python", "./prog/py_test.py"],
                           name="test-kube",
-                          # secrets=[secret_env],
+                          secrets=[secret_env],
                           in_cluster=True, #To trigger cluster kubeconfig.
                           image_pull_policy="Always",  #In my case, I need the image update to occur whenever there is an update
                           task_id="py_test_k8_task2",
